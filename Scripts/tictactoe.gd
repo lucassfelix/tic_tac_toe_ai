7f2fbdc30ebf6930daf203b523c16061dc11b8db
Lucas Félix <lucas.salaverry@edu.pucrs.br>
2022-08-16 22:24:45 -0300

    Versão jogável jogo da velha com Minimax

--- Scripts/tictactoe.gd
+++ Scripts/tictactoe.gd
@@ -3,14 +3,11 @@ extends Node2D
 enum BOARD_STATE {EMPTY= 0, X=-1, O=1};
 var board: Array;
 var ai_symbol : int = BOARD_STATE.X;
-var debug_mode : bool = false;
-var textInput : TextEdit;
 
 func _ready() -> void:
-	for i in range(9):
+	for _i in range(9):
 		board.append(BOARD_STATE.EMPTY);
 	drawDebugBoard(board);
-	textInput = $TextEdit;
 	pass
 	
 func drawDebugBoard(aux_board):
@@ -27,18 +24,12 @@ func drawDebugBoard(aux_board):
 	print()
 
 func endgame(c_board):
-	if c_board[0] != BOARD_STATE.EMPTY and	c_board[1] != BOARD_STATE.EMPTY and c_board[2] != BOARD_STATE.EMPTY and c_board[0] == c_board[1] and c_board[1] == c_board[2]:
-		return 1 if c_board[0] == ai_symbol else -1 
-	if c_board[3] != BOARD_STATE.EMPTY and	c_board[4] != BOARD_STATE.EMPTY and c_board[5] != BOARD_STATE.EMPTY and c_board[3] == c_board[4] and c_board[4] == c_board[5]:
-		return 1 if c_board[3] == ai_symbol else -1 
-	if c_board[6] != BOARD_STATE.EMPTY and	c_board[7] != BOARD_STATE.EMPTY and c_board[8] != BOARD_STATE.EMPTY and c_board[6] == c_board[7] and c_board[7] == c_board[8]:
-		return 1 if c_board[6] == ai_symbol else -1 
-	if c_board[0] != BOARD_STATE.EMPTY and	c_board[3] != BOARD_STATE.EMPTY and c_board[6] != BOARD_STATE.EMPTY and c_board[0] == c_board[3] and c_board[3] == c_board[6]:
-		return 1 if c_board[0] == ai_symbol else -1 
-	if c_board[1] != BOARD_STATE.EMPTY and	c_board[4] != BOARD_STATE.EMPTY and c_board[7] != BOARD_STATE.EMPTY and c_board[1] == c_board[4] and c_board[4] == c_board[7]:
-		return 1 if c_board[1] == ai_symbol else -1 
-	if c_board[2] != BOARD_STATE.EMPTY and	c_board[5] != BOARD_STATE.EMPTY and c_board[8] != BOARD_STATE.EMPTY and c_board[2] == c_board[5] and c_board[5] == c_board[8]:
-		return 1 if c_board[2] == ai_symbol else -1 
+	for row in range(3):
+		if c_board[row] != BOARD_STATE.EMPTY and	c_board[row+1] != BOARD_STATE.EMPTY and c_board[row+2] != BOARD_STATE.EMPTY and c_board[row] == c_board[row+1] and c_board[row+1] == c_board[row+2]:
+			return 1 if c_board[row] == ai_symbol else -1
+	for col in range(3):
+		if c_board[col] != BOARD_STATE.EMPTY and	c_board[col+3] != BOARD_STATE.EMPTY and c_board[col+6] != BOARD_STATE.EMPTY and c_board[col] == c_board[col+3] and c_board[col+3] == c_board[col+6]:
+			return 1 if c_board[col] == ai_symbol else -1  
 	if c_board[0] != BOARD_STATE.EMPTY and	c_board[4] != BOARD_STATE.EMPTY and c_board[8] != BOARD_STATE.EMPTY and c_board[0] == c_board[4] and c_board[4] == c_board[8]:
 		return 1 if c_board[0] == ai_symbol else -1 
 	if c_board[6] != BOARD_STATE.EMPTY and	c_board[4] != BOARD_STATE.EMPTY and c_board[2] != BOARD_STATE.EMPTY and c_board[6] == c_board[4] and c_board[4] == c_board[2]:
@@ -50,7 +41,7 @@ func endgame(c_board):
 	return 2
 
 func geraRamificacoes(aux_board : Array, ai_turn : bool) -> Array:
-	var branches : Array;
+	var branches : Array = [];
 	for index in range(9):
 		if aux_board[index] == BOARD_STATE.EMPTY:
 			var new_board = aux_board.duplicate(true);
@@ -64,26 +55,17 @@ func minimax(aux_board : Array, ai_turn : bool) -> Array:
 	var end : int = endgame(aux_board);
 	if end != 2:
 		#Se o jogo acabou, retorna o score do resultado final.
-		if debug_mode:
-			print("ENDGAME Value: " + str(end));
-		return [end, aux_board];
+		return [end, [aux_board]];
 		
 	var valor : int;
-	var bestBranches : Array;
+	var bestBranches : Array = [];
 	
 	var branches = geraRamificacoes(aux_board,ai_turn);
-	if debug_mode:
-		print("\nBranches\n")
-		for b in branches:
-			drawDebugBoard(b)
-			
-	var debugValores : Array;
 	if ai_turn:
 		#Se for o turno da IA
 		valor = -9;
 		for new_board in branches:
 			var new_value : int = minimax(new_board,!ai_turn)[0];
-			debugValores.append(new_value)
 			
 			if new_value == valor:
 				bestBranches.append(new_board)
@@ -97,25 +79,13 @@ func minimax(aux_board : Array, ai_turn : bool) -> Array:
 		valor = 9;
 		for new_board in branches:
 			var new_value : int = minimax(new_board,!ai_turn)[0];
-			debugValores.append(new_value)
 			if new_value == valor:
 				bestBranches.append(new_board)
 			elif  new_value < valor:
 				bestBranches.clear()
 				bestBranches.append(new_board)
 				valor = new_value
-			
-	if debug_mode:
-		print("\nMinimax Result\n");
-		drawDebugBoard(aux_board);
-		print("Turno da IA? " + str(ai_turn))
-		print(debugValores)
-		print(valor)
 	return [valor,bestBranches];
-
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_Button_pressed() -> void:
 	var play = $TextEdit.text as int;
